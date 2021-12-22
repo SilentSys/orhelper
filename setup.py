1fcd9a4b438ab398f1c77dc0a69b5812a9db50d5
Andrei Popescu <Popescu.Andrei.David@gmail.com>
2021-12-21 16:15:35 -0800

    Bumped to v0.1.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="orhelper",
-    version="0.1.2",
+    version="0.1.3",
     author="Andrei Popescu",
     author_email="Popescu.Andrei.David@gmail.com",
     description="OrHelper is a module which aims to facilitate interacting and scripting with OpenRocket from Python.",
