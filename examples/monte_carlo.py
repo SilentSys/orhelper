6fb7aa3f789455c54a37e3c94a3838ce83ce2e89
SiboVG <sibo.vangool@hotmail.com>
2022-02-11 04:09:03 +0100

    Change simple.ork load location in examples

--- examples/monte_carlo.py
+++ examples/monte_carlo.py
@@ -17,7 +17,7 @@ class LandingPoints(list):
 
             # Load the document and get simulation
             orh = orhelper.Helper(instance)
-            doc = orh.load_doc(os.path.join('examples', 'simple.ork'))
+            doc = orh.load_doc('simple.ork')
             sim = doc.getSimulation(0)
 
             # Randomize various parameters
